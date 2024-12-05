5ec557a64f10a376d5d82c63cd4af8b8dfa8c339
KNXCSdev <michal.miksho@gmail.com>
2024-12-05 20:44:36 +0100

    Script,Media Queries

---
+++ main.js
@@ -0,0 +1,26 @@
+// Theme Switcher
+let darkMode = localStorage.getItem("darkMode");
+const darkModeSwitcher = document.getElementById("dark-mode-switch");
+
+const enabledarkMode = () => {
+  document.body.classList.add("darkMode");
+  localStorage.setItem("darkMode", "enabled");
+};
+
+const disabledarkMode = () => {
+  document.body.classList.remove("darkMode");
+  localStorage.setItem("darkMode", "disabled");
+};
+
+if (darkMode === "enabled") {
+  enabledarkMode();
+}
+
+darkModeSwitcher.addEventListener("change", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enabledarkMode();
+  } else {
+    disabledarkMode();
+  }
+});
